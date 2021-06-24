a64aa2b4850838aaf9010004b4a553035f1b4ada
Juan408AR <aguirre.juanmp15@gmail.com>
2021-06-23 22:54:23 -0700

    Added button handler feature

--- Ch15/src/sample/Main.java
+++ Ch15/src/sample/Main.java
@@ -1,13 +1,13 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.shape.Line;
-import javafx.scene.layout.Pane;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 
 
@@ -15,22 +15,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Scene scene = new Scene(new LinePane(), 200, 200);
-        primaryStage.setTitle("ShowLine");
+        HBox pane = new HBox(10);
+        pane.setAlignment(Pos.CENTER);
+        Button btOk = new Button("OK");
+        Button btCancel = new Button("Cancel");
+        OKHandlerClass handler1 = new OKHandlerClass();
+        CancelHandlerClass handler2 = new CancelHandlerClass();
+
+        // when button is clicked it triggers the handler
+        // the arg class is of type event-handler so it is able to be passed into this method that accepts only event-handler types
+        btOk.setOnAction(handler1);
+        btCancel.setOnAction(handler2);
+
+        pane.getChildren().addAll(btOk, btCancel);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Event buttons");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
-    class LinePane extends Pane{
-        public LinePane(){
-            Line line1 = new Line(10, 10, 10, 10);
-            line1.endYProperty().bind(widthProperty().subtract(10));
-            line1.endXProperty().bind(heightProperty().subtract(10));
-            line1.setStrokeWidth(5);
-            line1.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
-            getChildren().add(line1);
 
-        }
+
     }
 
 
@@ -38,3 +43,23 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+class OKHandlerClass implements EventHandler<ActionEvent>{
+    @Override
+    public void handle(ActionEvent e){
+        System.out.println("Ok Button Clicked");
+    }
+}
+
+class CancelHandlerClass implements EventHandler<ActionEvent>{
+    @Override
+    public void handle(ActionEvent e){
+        System.out.println("Cancel Button Clicked");
+    }
+}
+
+// Ch15: Event-Driven Programming and Animation
+// 15.1: Button -> Event -> Handler
+// Buttons are an event source that can trigger an event,
+// the event is then handled by the handler. You also need to have an object
+// be able to handle an event it must extend the EventHandler class
