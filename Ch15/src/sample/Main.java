b0f0e9d161eb04463a47b72f28c6670f8a368a2b
Juan Aguirre <aguirre.juanmp15@gmail.com>
2021-06-25 00:20:49 -0700

    Added circle without event control

--- Ch15/src/sample/Main.java
+++ Ch15/src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.sun.source.tree.BreakTree;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,32 +9,60 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane; // using this to organize other panes/nodes
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        HBox pane = new HBox(10);
-        pane.setAlignment(Pos.CENTER);
-        Button btOk = new Button("OK");
-        Button btCancel = new Button("Cancel");
-        OKHandlerClass handler1 = new OKHandlerClass();
-        CancelHandlerClass handler2 = new CancelHandlerClass();
+//        HBox pane = new HBox(10);
+//        pane.setAlignment(Pos.CENTER);
+//        Button btOk = new Button("OK");
+//        Button btCancel = new Button("Cancel");
+//        OKHandlerClass handler1 = new OKHandlerClass();
+//        CancelHandlerClass handler2 = new CancelHandlerClass();
+//
+//        // when button is clicked it triggers the handler
+//        // the arg class is of type event-handler so it is able to be passed into this method that accepts only event-handler types
+//        btOk.setOnAction(handler1);
+//        btCancel.setOnAction(handler2);
+//
+//        pane.getChildren().addAll(btOk, btCancel);
+//
+//        Scene scene = new Scene(pane);
+//        primaryStage.setTitle("Event buttons");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
 
-        // when button is clicked it triggers the handler
-        // the arg class is of type event-handler so it is able to be passed into this method that accepts only event-handler types
-        btOk.setOnAction(handler1);
-        btCancel.setOnAction(handler2);
+        // 15.3
+        StackPane pane = new StackPane();
+        Circle circle = new Circle(50);
+        circle.setStroke(Color.BLUEVIOLET);
+        circle.setFill(Color.WHITE);
+        pane.getChildren().add(circle);
 
-        pane.getChildren().addAll(btOk, btCancel);
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(pane);
-        primaryStage.setTitle("Event buttons");
+        Button btEnlarge = new Button("Enlarge");
+        Button btShrink = new Button("Shrink");
+
+        hBox.getChildren().addAll(btEnlarge, btShrink);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
+        borderPane.setBottom(hBox);
+        BorderPane.setAlignment(hBox, Pos.CENTER); // applies to anything that is a BorderPane? I think yes
+
+        Scene scene = new Scene(borderPane, 200, 150); // notice how we had multple panes put into one pane in order to pass them into this scene
+        primaryStage.setTitle("CircleWithoutEvents");
         primaryStage.setScene(scene);
         primaryStage.show();
-
 
 
     }
@@ -73,3 +102,12 @@ class CancelHandlerClass implements EventHandler<ActionEvent>{
 // The children can fire events if their parent is able to fire events
 // there are methods and classes for various events and even one to find the source event
 
+// 15.3 Registering Handlers and Handling Events:
+// Events must be linked to event sources and the event handler must implement the appropriate interface for events
+// The event listeners/handlers are an instance of the interface they implement
+// The source must set the handler/listener which is fired on order from the source
+// ex...Button ok = new button("ok"); // creating an event source
+//      OKHandler handler1 = new OKHandler() // creating an event handler
+//      ok.setOnAction(handler1) // button sets handler as delegate
+
+//
